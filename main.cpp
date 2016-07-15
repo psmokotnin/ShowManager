8f6bc69bcbca147ee8732c39680987136ea86ef7
psmokotnin <p.smokotnin@gmail.com>
2016-07-15 10:25:19 +0300

    add main

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "smapplication.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    SMApplication app;
+
+    app.showWindow();
+    return a.exec();
+}
